ececb3d645d463c86280f6e6f962b22eebaf542c
Karen Li <kli19@stuy.edu>
2019-05-13 01:47:05 -0400

    finished?

--- script.py
+++ script.py
@@ -46,6 +46,74 @@ def run(filename):
                           'blue': [0.2, 0.5, 0.5]}]
     reflect = '.white'
 
-    print symbols
+    print (symbols)
     for command in commands:
         print command
+
+        c = command['op']
+        args = command['args']
+        if c == 'push':
+            stack.append([x[:] for x in stack[-1]])
+            
+        elif c == 'pop':
+            stack.pop()
+            
+        elif c == 'move':
+            tmp = make_translate(args[0], args[1], args[2])
+            matrix_mult(stack[-1], tmp)
+            stack[-1] = [x[:] for x in tmp]
+            tmp = []
+
+        elif c == 'rotate':
+            theta = args[1] * (math.pi/180)
+            if args[0] == 'x':
+                tmp = make_rotX(theta)
+            elif args[0] == 'y':
+                tmp = make_rotY(theta)
+            else:
+                tmp = make_rotZ(theta)
+            matrix_mult( stack[-1], tmp )
+            stack[-1] = [ x[:] for x in tmp]
+            tmp = []
+            
+        elif c == 'scale':
+            tmp = make_scale(args[0], args[1], args[2])
+            matrix_mult(stack[-1], tmp)
+            stack[-1] = [x[:] for x in tmp]
+            tmp = []     
+
+        elif c == 'box':
+            add_box(tmp,
+                    args[0], args[1], args[2],
+                    args[3], args[4], args[5])
+            matrix_mult( stack[-1], tmp )
+            draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, reflect)
+            tmp = []
+
+        elif c == 'sphere':
+            add_sphere(tmp,
+                       args[0], args[1], args[2], args[3], step_3d)
+            matrix_mult( stack[-1], tmp )
+            draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, reflect)
+            tmp = []
+
+        elif c == 'torus':
+            add_torus(tmp,
+                      args[0], args[1], args[2], args[3], args[4], step_3d)
+            matrix_mult( stack[-1], tmp )
+            draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, reflect)
+            tmp = []
+            
+        elif c == 'line':
+            add_edge(tmp,
+                     args[0], args[1], args[2], args[3], args[4], args[5])
+            matrix_mult( stack[-1], tmp )
+            draw_lines(tmp, screen, zbuffer, color)
+            tmp = []
+            
+        elif c == 'save':
+            save_extension(screen, args[0])
+            
+        elif c == 'display':
+            display(screen)
+
